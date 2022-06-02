a019e9955d92769b9600609f1c45c99b160fe6c7
EC2 Default User <ec2-user@ip-172-31-21-212.ap-northeast-1.compute.internal>
2022-06-02 15:09:43 +0000

    ゲストログイン機能修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,4 @@
 class HomeController < ApplicationController
   def index
   end
-  
-  def new_guest
-    user = User.guest #ゲストユーザーをモデルから呼び出す
-    sign_in user #ゲストユーザーでサインイン
-    redirect_to root_path
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,10 +11,10 @@ class User < ApplicationRecord
   validates :username, presence: true
   
   def self.guest 
-    find_or_create_by(email: 'guest@example.com',
-                       encrypted_password: 1234567,
-                      ) do |user|
-      user.password = SecureRandom.urlsafe_base64 #パスワードはランダム生成
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
+      # 例えば name を入力必須としているならば， user.name = "ゲスト" なども必要
     end
   end
 
