ef45635a42d52a39fa907f297f7b6d0f6da593ac
EC2 Default User <ec2-user@ip-172-31-21-212.ap-northeast-1.compute.internal>
2022-06-14 21:46:45 +0000

    test修正

--- spec/factories/mountains.rb
+++ spec/factories/mountains.rb
@@ -7,8 +7,9 @@ FactoryBot.define do
     endtime { "15:47:21" }
     food { "3"}
     water { 3 }
-    after(:build) do |post|
-      post.image.attach(io: File.open('spec/fixtures/images/test.jpg'), filename: 'test.jpg', content_type: 'image/jpg')
+    after(:build) do |item|
+    # ActiveStorageの場合
+    item.image.attach(io: File.open('spec/fixtures/test_image.jpg'), filename: 'test_image.jpg', content_type: 'image/jpg')
     end
   end
 end
