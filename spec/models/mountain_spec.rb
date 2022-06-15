5f35ad063384e618c7cde5255944d30d5dd1149c
EC2 Default User <ec2-user@ip-172-31-21-212.ap-northeast-1.compute.internal>
2022-06-15 02:55:16 +0000

    test修正

--- spec/models/mountain_spec.rb
+++ spec/models/mountain_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Mountain, type: :model do
   let!(:mountain) { create(:mountain) }
- 
   
   it '山の名前を取得できる' do
     expect(mountain.name).to include mountain.name
