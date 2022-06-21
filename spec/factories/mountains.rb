14c312d857e993689d74aa18a6d25e96e8a87c8f
EC2 Default User <ec2-user@ip-172-31-21-212.ap-northeast-1.compute.internal>
2022-06-21 13:47:21 +0000

    mountainのテスト追加

--- spec/factories/mountains.rb
+++ spec/factories/mountains.rb
@@ -5,8 +5,8 @@ FactoryBot.define do
     season { "冬" }
     start { "13:47:21" }
     endtime { "15:47:21" }
-    food { "3"}
-    water { 3 }
+    food { "3" }
+    water { "3" }
     after(:build) do |item|
     # ActiveStorageの場合
     item.image.attach(io: File.open('spec/fixtures/test_image.jpg'), filename: 'test_image.jpg', content_type: 'image/jpg')
--- spec/models/mountain_spec.rb
+++ spec/models/mountain_spec.rb
@@ -7,5 +7,12 @@ RSpec.describe Mountain, type: :model do
     expect(mountain.name).to include mountain.name
   end
   
-
+  it '季節を取得できる' do
+    expect(mountain.season).to include mountain.season
+  end
+  
+  it '食料を取得できる' do
+    expect(mountain.food).to include mountain.food
+  end
+  
 end
